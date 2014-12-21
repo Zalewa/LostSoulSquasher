bd4b99af337c5d08b0a127a54c9841579b2df50b
Zalewa <zalewapl@gmail.com>
2014-12-21 09:09:01 +0100

    Moved out AnimationFrame to its own file.

--- LostSoul/AnimationBehavior.cs
+++ LostSoul/AnimationBehavior.cs
@@ -7,34 +7,6 @@ using System.Text;
 
 namespace LostSoul
 {
-    public class AnimationFrame
-    {
-        public Texture2D Texture;
-        public Vector2 Origin = Vector2.Zero;
-
-        public AnimationFrame(Texture2D texture)
-        {
-            this.Texture = texture;
-        }
-
-        public static AnimationFrame MkCentered(Texture2D texture)
-        {
-            var result = new AnimationFrame(texture);
-            result.Origin = new Vector2(texture.Width / 2, texture.Height / 2);
-            return result;
-        }
-
-        public static AnimationFrame[] MkCentered(Texture2D[] textures)
-        {
-            var result = new AnimationFrame[textures.Length];
-            for (int i = 0; i < textures.Length; ++i)
-            {
-                result[i] = MkCentered(textures[i]);
-            }
-            return result;
-        }
-    }
-
     public class AnimationBehavior : Behavior
     {
         private AnimationFrame[] frames;
---
+++ LostSoul/AnimationFrame.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LostSoul
+{
+    public class AnimationFrame
+    {
+        public Texture2D Texture;
+        public Vector2 Origin = Vector2.Zero;
+
+        public AnimationFrame(Texture2D texture)
+        {
+            this.Texture = texture;
+        }
+
+        public static AnimationFrame MkCentered(Texture2D texture)
+        {
+            var result = new AnimationFrame(texture);
+            result.Origin = new Vector2(texture.Width / 2, texture.Height / 2);
+            return result;
+        }
+
+        public static AnimationFrame[] MkCentered(Texture2D[] textures)
+        {
+            var result = new AnimationFrame[textures.Length];
+            for (int i = 0; i < textures.Length; ++i)
+            {
+                result[i] = MkCentered(textures[i]);
+            }
+            return result;
+        }
+    }
+}
