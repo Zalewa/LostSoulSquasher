c48666a98de00b7ea28b6c1476741784fea75307
Zalewa <zalewapl@gmail.com>
2014-12-21 20:44:41 +0100

    Randomize background every 20 seconds.

--- LostSoul/BackgroundAnimationBehavior.cs
+++ LostSoul/BackgroundAnimationBehavior.cs
@@ -8,16 +8,48 @@ namespace LostSoul
 {
     public class BackgroundAnimationBehavior : Behavior
     {
+        private const float backgroundChangeInterval = 20.0f;
+
         private Entity entity;
+        private float countdownTillBackgroundChange = 0.0f;
+        private int backgroundIndex = -1;
+        private Random random = new Random();
 
         public BackgroundAnimationBehavior(Entity entity)
         {
             this.entity = entity;
             entity.Game.World.GameOverChanged += GameOverChangedHandler;
+            RandomizeBackgroundIndex(entity);
         }
+
 
         public override void Run(GameTime gameTime, Entity entity)
         {
+            if (!entity.Game.World.IsGameOver())
+            {
+                countdownTillBackgroundChange -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (countdownTillBackgroundChange < 0.0f)
+                {
+                    SwitchToNextBackground(entity);
+                    countdownTillBackgroundChange = backgroundChangeInterval;
+                }
+            }
+        }
+
+        private void SwitchToNextBackground(Entity entity)
+        {
+            var render = (SpriteRenderBehavior)entity.RenderBehavior;
+            RandomizeBackgroundIndex(entity);
+            render.Texture = entity.Game.ContentLoader.Backgrounds[backgroundIndex];
+        }
+
+        private void RandomizeBackgroundIndex(Entity entity)
+        {
+            int currentIndex = backgroundIndex;
+            while (backgroundIndex == currentIndex)
+            {
+                backgroundIndex = random.Next(entity.Game.ContentLoader.Backgrounds.Length);
+            }
         }
 
         private void GameOverChangedHandler(object sender, EventArgs e)
