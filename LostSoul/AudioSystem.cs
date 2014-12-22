45ea7b7a718fa6274ee0f993f2493c6a09a0e174
Zalewa <zalewapl@gmail.com>
2014-12-22 16:48:45 +0100

    Allow playing non-positioned sound & don't dispose sounds before they stop.

--- LostSoul/AudioSystem.cs
+++ LostSoul/AudioSystem.cs
@@ -11,19 +11,28 @@ namespace LostSoul
     class SoundRequest
     {
         public SoundEffect Sound { get; set; }
-        public Vector2 Position { get; set; }
+        public Vector2? Position { get; set; }
     };
 
     public class AudioSystem
     {
         private Random random = new Random();
         private List<SoundRequest> soundRequests = new List<SoundRequest>();
+        private List<SoundEffectInstance> playingSounds = new List<SoundEffectInstance>();
         public Vector2 ListenerPosition = Vector2.Zero;
         public float PanDivisor = 1.0f;
         public float PanClamp = 1.0f;
 
         public AudioSystem()
         {
+        }
+
+        public void PlaySound(SoundEffect sound)
+        {
+            var request = new SoundRequest();
+            request.Sound = sound;
+            request.Position = null;
+            soundRequests.Add(request);
         }
 
         public void PlaySound(SoundEffect sound, Vector2 position)
@@ -34,20 +43,37 @@ namespace LostSoul
             soundRequests.Add(request);
         }
 
-
         public void FireSounds()
         {
             soundRequests.ForEach(e => FireSound(e));
             soundRequests.Clear();
+            ClearCompletedSounds();
         }
 
         private void FireSound(SoundRequest request)
         {
             SoundEffectInstance instance = request.Sound.CreateInstance();
-            Vector2 relative = request.Position - ListenerPosition;
-            instance.Pan = MathHelper.Clamp(relative.X / PanDivisor, -PanClamp, PanClamp);
+            if (request.Position != null)
+            {
+                Vector2 relative = (Vector2)request.Position - ListenerPosition;
+                instance.Pan = MathHelper.Clamp(relative.X / PanDivisor, -PanClamp, PanClamp);
+            }
             instance.Pitch = 0.5f - (float)random.NextDouble();
             instance.Play();
+            playingSounds.Add(instance);
+        }
+
+        private void ClearCompletedSounds()
+        {
+            for (int i = 0; i < playingSounds.Count; ++i)
+            {
+                if (playingSounds[i].State == SoundState.Stopped)
+                {
+                    playingSounds[i].Dispose();
+                    playingSounds.RemoveAt(i);
+                    --i;
+                }
+            }
         }
 
         public void PlayMusic(Song song)
